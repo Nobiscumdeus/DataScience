8a94aa966343d5134d62ecc27125954a00a5d943
Nobiscumdeus <adeolaolumide99@gmail.com>
2024-06-21 17:34:23 +0100

    updates

--- Streamlit/basic.py
+++ Streamlit/basic.py
@@ -1,32 +1,57 @@
-import yfinance #Also install it 
-import streamlit as st 
+import streamlit as st
 import pandas as pd 
-
-st.write(""" 
-
-#Simple Stock Price App
-
-Shown are the stock closing price and volume of Google!
-
-         
-         """)
-tickerSymbol='GOOGL'
-#Get data on this ticker 
-tickerData=yf.Ticker(tickerSymbol)
-#Get the historical prices of this ticker 
-tickerDf=tickerData.history(period='id',start='2010-5-31',end='2020-5-31')
-#Open high low close volume dividends stock splits 
-
-
-st.line_chart(tickerDf.close)
-st.line_chart(tickerDf.volume)
+import matplotlib.pyplot as plt 
+import seaborn as sns 
 
 
 
+#Load sample dataset or utilize your own dataset 
+@st.cache
 
+def load_data():
+    return sns.load_dataset('iris')
 
+#Main function to define the Streamlit app 
+def main():
+    st.title('Exploratory Data analysis dashboard (EDA')
+    
+    #Load dataset 
+    data=load_data()
+    
+    #Display the dataset 
+    st.subheader("Dataset")
+    st.write(data)
+    
+    #Interactive widgets for filtering data
+    species=st.sidebar.multiselect(
+    'Select Species',data['species'].unique()
+    )
+    sepal_length_range = st.sidebar.slider('Select Sepal Length Range', float(data['sepal_length'].min()), float(data['sepal_length'].max()), (float(data['sepal_length'].min()), float(data['sepal_length'].max())))
+      
+     # Filter data based on user inputs
+    filtered_data = data[(data['species'].isin(species)) & 
+                         (data['sepal_length'] >= sepal_length_range[0]) & 
+                         (data['sepal_length'] <= sepal_length_range[1])]
+    
+    # Display filtered data
+    st.subheader('Filtered Data')
+    st.write(filtered_data)
+    
+    # Visualization section
+    st.subheader('Data Visualization')
+    
+    # Histogram of Sepal Length
+    st.write('## Histogram of Sepal Length')
+    plt.figure(figsize=(8, 6))
+    sns.histplot(data=filtered_data, x='sepal_length', bins=20, kde=True)
+    st.pyplot()
+    
+    # Scatter plot of Sepal Length vs Sepal Width
+    st.write('## Scatter Plot of Sepal Length vs Sepal Width')
+    plt.figure(figsize=(8, 6))
+    sns.scatterplot(data=filtered_data, x='sepal_length', y='sepal_width', hue='species')
+    st.pyplot()
 
-
-
-v
-
+# Run the app
+if __name__ == '__main__':
+    main() 
